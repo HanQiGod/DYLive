d3fa3bbf89c506c22c5d297eaef07c309b342f14
HanQiGod <1539841933@qq.com>
2019-04-12 17:03:08 +0800

    初始化项目

--- DYLive/AppDelegate.swift
+++ DYLive/AppDelegate.swift
@@ -2,8 +2,10 @@
 //  AppDelegate.swift
 //  DYLive
 //
-//  Created by 君凯商联网 on 2019/4/12.
-//  Copyright © 2019 君凯商联网. All rights reserved.
+//  Created by Mr_Han on 2019/4/12.
+//  Copyright © 2019 Mr_Han. All rights reserved.
+//  CSDN <https://blog.csdn.net/u010960265>
+//  GitHub <https://github.com/HanQiGod>
 //
 
 import UIKit
